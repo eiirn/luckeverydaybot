ad168cd6e2c63e26ddc6a4e96228c1a5db5b1b32
Eirene <eirnnceleste@gmail.com>
2025-03-27 11:15:54 +0530

    🔔 Enhance retention notification service with logging and error handling

--- lib/scheduled/retention_notification.dart
+++ lib/scheduled/retention_notification.dart
@@ -4,14 +4,28 @@ import 'package:televerse/televerse.dart';
 
 import '../database/user_methods.dart';
 import '../luckeverydaybot.dart';
+import '../utils/logger.dart';
+
+class _FailedNotification {
+  const _FailedNotification(this.id, this.reason);
+  final int id;
+  final String reason;
+
+  @override
+  String toString() => '$id: $reason\n\n';
+}
 
 class RetentionNotificationService {
   Future<void> notifyInactiveUsers() async {
+    BotLogger.log('🔔 Running retention notifications').ignore();
     final methods = UserMethods(supabase);
 
     final peeps = await methods.fetchUsersForReminder();
-    log('We have ${peeps.length} number of people to notify.');
-
+    BotLogger.log(
+      'We have ${peeps.length} number of people to notify.\n\n[${peeps.map((e) => e.userId).join(', ')}]',
+    ).ignore();
+    var success = 0;
+    final failed = <_FailedNotification>[];
     for (var i = 0; i < peeps.length; i++) {
       try {
         await api.sendMessage(
@@ -20,8 +34,15 @@ class RetentionNotificationService {
         );
         await methods.updateLastNotified(peeps[i].userId);
         log('Notified ${i + 1}/${peeps.length}: ${peeps[i].userId}');
+        success++;
         await Future.delayed(const Duration(seconds: 8));
-      } catch (_) {}
+      } catch (err) {
+        failed.add(_FailedNotification(peeps[i].userId, err.toString()));
+      }
     }
+    BotLogger.log(
+      'Total: ${peeps.length}\nSuccess: $success\nFailed: ${failed.length}',
+    ).ignore();
+    log(failed.toString());
   }
 }
